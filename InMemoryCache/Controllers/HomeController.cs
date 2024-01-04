31b5bbeb79699237d6ae5d5b8868f18dc858ead1
Amir Doosti <amirdoosti@hotmail.com>
2024-01-04 21:22:51 +0400

    Add project files.

---
+++ InMemoryCache/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using InMemoryCache.Models;
+using InMemoryCache.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InMemoryCache.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly CachingService _cachingService;
+
+        public HomeController(ILogger<HomeController> logger, CachingService cachingService)
+        {
+            _logger = logger;
+            _cachingService = cachingService;
+        }
+
+        public IActionResult Index()
+        {
+            // Use the caching service to get or set cached data
+            var cachedData = _cachingService.GetOrSetCachedData();
+
+            return View(model: cachedData);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InMemoryCache/Services/CachingService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace InMemoryCache.Services
+{
+    public class CachingService
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public CachingService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public string GetOrSetCachedData()
+        {
+            // Check if the data is already in the cache
+            if (_memoryCache.TryGetValue<string>("cachedData", out var cachedData))
+            {
+                return cachedData;
+            }
+
+            // If not in the cache, fetch the data (simulated data in this example)
+            var newData = "This data is fetched at: " + DateTime.Now.ToString();
+
+            // Set the data in the cache with a specified expiration time
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(10)
+            };
+
+            _memoryCache.Set("cachedData", newData, cacheEntryOptions);
+
+            return newData;
+        }
+    }
+}
